d1cdc3c0e4c274705560b0a1d8878c3e0f379804
Roberto Fuentes <155397372+roberfg@users.noreply.github.com>
2026-08-12 00:13:22 -0400

    feat: Update theme (vscode, opencode, wezterm)

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -1,13 +1,29 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-if wezterm.target_os == 'windows' then
+if wezterm.target_triple:match('windows') then
     config.default_prog = { 'pwsh.exe' }
+    config.initial_cols = 130
+    config.initial_rows = 35
+
+    wezterm.on('gui-startup', function(cmd)
+        local _, _, mux_window = wezterm.mux.spawn_window(cmd or {})
+        local gui_window = mux_window:gui_window()
+        local screen = wezterm.gui.screens().active
+        local dimensions = gui_window:get_dimensions()
+
+        gui_window:set_position(
+            screen.x + math.floor((screen.width - dimensions.pixel_width) / 2),
+            screen.y + math.floor((screen.height - dimensions.pixel_height) / 2)
+        )
+
+        return mux_window
+    end)
 else
     config.default_prog = { 'bash', '-l' }
 end
 
-config.font_size = 12
+config.font_size = 11
+config.color_scheme = 'OneDark (base16)'
 
-wezterm.apply_config_to_all_screens(config)
-wezterm.apply_config(config)
+return config
